4e7414943fa07f7fa2c185a3aaed7b786982d2ca
chanieasmamaw-179 <chanieasmamaw@yahoo.com>
2026-05-06 22:24:44 +0200

    Clean repo: remove venv, buildozer, external files

--- ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/utils/quant_levels_utils.o.d
+++
@@ -1,8 +0,0 @@
-/Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/utils/quant_levels_utils.o: \
-  /Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/quant_levels_utils.c \
-  /Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/quant_levels_utils.h \
-  /Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/utils/quant_levels_utils.h:
-
-/Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
--- ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/dists/coffeegame/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/Users/masterschool/Desktop/New_Project_Ethiopian_2026/ethiopian_coffee_game/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/cpufeatures/cpu-features.o: \
-  /Users/masterschool/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.c \
-  /Users/masterschool/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h
-
-/Users/masterschool/.buildozer/android/platform/android-ndk-r25b/sources/android/cpufeatures/cpu-features.h:
